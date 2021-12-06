68d66dc7044e1e79b2d7133ed4943654afb64608
Dan Chalmers <dan@danchalmers.me.uk>
2021-12-06 09:20:23 +0000

    a little tidier

--- day_five.py
+++ day_five.py
@@ -6,6 +6,8 @@ from pipe import map, where, chain
 TEST_FILE = 'test-5.txt'
 REAL_FILE = "input-5.txt"
 
+Coord = tuple[int, int]
+
 
 def _read_file(file_name: str) -> Iterable[str]:
     with open('data/' + file_name, 'r') as f:
@@ -14,68 +16,63 @@ def _read_file(file_name: str) -> Iterable[str]:
                 yield line
 
 
-def _pair_from_str(pair_text: str) -> tuple[int, int]:
+def _pair_from_str(pair_text: str) -> Coord:
     xy = pair_text.strip().split(',')
     return int(xy[0]), int(xy[1])
 
 
-def _make_line(end1: tuple[int, int], end2: tuple[int, int]) -> list[tuple[int, int]]:
+def _make_line(end1: Coord, end2: Coord) -> list[Coord]:
+    x_step = 1 if end1[0] < end2[0] else -1
+    y_step = 1 if end1[1] < end2[1] else -1
     if end1[0] == end2[0]:
-        return [(end1[0], y) for y in range(min(end1[1], end2[1]), max(end1[1], end2[1])+1)]
-    else:
-        return [(x, end1[1]) for x in range(min(end1[0], end2[0]), max(end1[0], end2[0])+1)]
-
-
-def _make_line_part2(end1: tuple[int, int], end2: tuple[int, int]) -> list[tuple[int, int]]:
-    if end1[0] == end2[0]:
-        return [(end1[0], y) for y in range(min(end1[1], end2[1]), max(end1[1], end2[1])+1)]
+        return [(end1[0], y) for y in range(end1[1], end2[1]+y_step, y_step)]
     elif end1[1] == end2[1]:
-        return [(x, end1[1]) for x in range(min(end1[0], end2[0]), max(end1[0], end2[0])+1)]
+        return [(x, end1[1]) for x in range(end1[0], end2[0]+x_step, x_step)]
     else:
-        x_step = 1 if end1[0] < end2[0] else -1
-        y_step = 1 if end1[1] < end2[1] else -1
-        return [(xy[0], xy[1])
-                for xy in zip(range(end1[0], end2[0]+x_step, x_step), range(end1[1], end2[1]+y_step, y_step))
-                ]
+        return [
+            (xy[0], xy[1])
+            for xy in zip(range(end1[0], end2[0]+x_step, x_step), range(end1[1], end2[1]+y_step, y_step))
+        ]
+
+
+def _file_to_points(file_name: str) -> list[tuple[Coord]]:
+    return list(
+        _read_file(file_name)
+        | map(lambda line: line.split('->'))
+        | map(lambda pairs: list(
+            pairs
+            | map(lambda pair: _pair_from_str(pair))
+        ))
+    )
+
+
+def _count_to_intersections(point_count: Counter) -> int:
+    point_count.subtract(set(point_count))
+    point_count = +point_count
+    intersections = len(point_count)
+    print(f"{intersections} intersections found")
+    return intersections
 
 
 def count_xy_intersections(file_name: str) -> int:
     point_count = Counter(
-        _read_file(file_name)
-        | map(lambda line: line.split('->'))
-        | map(lambda pairs: list(
-            pairs
-            | map(lambda pair: _pair_from_str(pair))
-        ))
+        _file_to_points(file_name)
         | where(lambda pairs: pairs[0][0] == pairs[1][0] or pairs[0][1] == pairs[1][1])
         | map(lambda pairs: _make_line(pairs[0], pairs[1]))
         | chain
     )
-    point_count.subtract(set(point_count))
-    point_count = +point_count
-    intersections = len(point_count)
-    print(f"{intersections} intersections found")
-    return intersections
+    return _count_to_intersections(point_count)
 
 
 def count_all_intersections(file_name: str) -> int:
     point_count = Counter(
-        _read_file(file_name)
-        | map(lambda line: line.split('->'))
-        | map(lambda pairs: list(
-            pairs
-            | map(lambda pair: _pair_from_str(pair))
-        ))
-        | map(lambda pairs: _make_line_part2(pairs[0], pairs[1]))
+        _file_to_points(file_name)
+        | map(lambda pairs: _make_line(pairs[0], pairs[1]))
         | chain
     )
-    point_count.subtract(set(point_count))
-    point_count = +point_count
-    intersections = len(point_count)
-    print(f"{intersections} intersections found")
-    return intersections
+    return _count_to_intersections(point_count)
 
 
 if __name__ == "__main__":
-    # count_xy_intersections(REAL_FILE)
+    count_xy_intersections(REAL_FILE)
     count_all_intersections(REAL_FILE)
