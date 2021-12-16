b5c8d03cae757f9bd988e4443d73b56074004fff
Dan Chalmers <dan@danchalmers.me.uk>
2021-12-16 12:43:11 +0000

    day 16 part one works

---
+++ day_sixteen.py
@@ -0,0 +1,139 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from pipe import reverse
+
+
+Bits = list[int]
+
+
+REAL_FILE = 'input-16.txt'
+
+
+def hex_to_bits(hex: str) -> Bits:
+    n = int(hex, 16)
+    return [(n >> position) & 1 for position in range((4 * len(hex)) - 1, -1, -1)]
+
+
+def bits_to_int(bits: Bits) -> int:
+    return sum([bit * 2**position for position, bit in enumerate(bits | reverse)])
+
+
+class Packet(ABC):
+    def __init__(self, version: int, type_id: int):
+        self.version = version
+        self.type_id = type_id
+
+    @abstractmethod
+    def value(self) -> int:
+        pass
+
+    @abstractmethod
+    def sub_packets(self) -> list[Packet]:
+        pass
+
+    @abstractmethod
+    def version_sum(self) -> int:
+        pass
+
+    @staticmethod
+    def bits_to_literal(bits: Bits) -> tuple[int, Bits]:
+        digits = []
+        index = 0
+        while (bits[index]) == 1:
+            digits.extend(bits[index + 1:index + 5])
+            index += 5
+        digits.extend(bits[index + 1:index + 5])
+        index += 5
+        return bits_to_int(digits), bits[index:]
+
+    @staticmethod
+    def make_packet_from_bin(bits: Bits) -> tuple[Optional[Packet], Bits]:
+        version = bits_to_int(bits[0:3])
+        type_value = bits_to_int(bits[3:6])
+        remaining = bits[6:]
+        if type_value == 4:
+            literal, remaining = Packet.bits_to_literal(remaining)
+            p = LiteralPacket(version, type_value, literal)
+        else:
+            p = OperatorPacket(version, type_value)
+            _, remaining = p.parse_sub_packets(remaining)
+        return p, remaining
+
+    @staticmethod
+    def make_packet_from_hex(hex: str) -> tuple[Packet, Bits]:
+        bits = hex_to_bits(hex)
+        return Packet.make_packet_from_bin(bits)
+
+
+class LiteralPacket(Packet):
+    def __init__(self, version: int, type_id: int, literal: int):
+        super().__init__(version, type_id)
+        self.literal = literal
+
+    def value(self) -> int:
+        return self.literal
+
+    def sub_packets(self) -> list[Packet]:
+        return []
+
+    def version_sum(self) -> int:
+        return self.version
+
+    def __repr__(self):
+        return f"Literal(v{self.version} = {self.value()})"
+
+
+class OperatorPacket(Packet):
+    def __init__(self, version: int, type_id: int):
+        super().__init__(version, type_id)
+        self.packets = []
+
+    def parse_sub_packets(self, remaining_bits: Bits) -> tuple[Packet, Bits]:
+        length_id = remaining_bits[0]
+        remaining_bits = remaining_bits[1:]
+        if length_id == 0:
+            length = bits_to_int(remaining_bits[:15])
+            remaining_bits = remaining_bits[15:]
+            sub_packet_bits = remaining_bits[:length]
+            remaining_bits = remaining_bits[length:]
+            while len(sub_packet_bits) > 1:
+                p, sub_packet_bits = Packet.make_packet_from_bin(sub_packet_bits)
+                self.packets.append(p)
+        else:
+            length = bits_to_int(remaining_bits[:11])
+            remaining_bits = remaining_bits[11:]
+            while len(self.packets) < length:
+                p, remaining_bits = Packet.make_packet_from_bin(remaining_bits)
+                self.packets.append(p)
+        return self, remaining_bits
+
+    def value(self) -> int:
+        return 0
+
+    def sub_packets(self) -> list[Packet]:
+        return self.packets
+
+    def version_sum(self) -> int:
+        return self.version + sum([p.version_sum() for p in self.sub_packets()])
+
+    def __repr__(self):
+        return f"Operator(v{self.version} = {self.sub_packets()})"
+
+
+def load_from_file(file_name: str) -> Packet:
+    with open('data/' + file_name, 'r') as f:
+        return Packet.make_packet_from_hex(f.readline().strip())
+
+
+def part_one(file_name: str):
+    packet, _ = load_from_file(file_name)
+    print(f"part one version sum: {packet.version_sum()}")
+
+
+
+
+if __name__ == "__main__":
+    part_one(REAL_FILE)
--- tests/test_sixteen.py
+++ tests/test_sixteen.py
@@ -1,8 +1,63 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+from day_sixteen import hex_to_bits, bits_to_int, Packet
+
+
+class DaySixteenTestCase(unittest.TestCase):
+    def test_hexbin(self):
+        self.assertEqual([0,0,0,0], hex_to_bits('0'))
+        self.assertEqual([1,0,1,0], hex_to_bits('A'))
+        self.assertEqual([1,1,1,0,0,0,0,1], hex_to_bits('E1'))
+
+    def test_binint(self):
+        self.assertEqual(0, bits_to_int([0,0,0,0]))
+        self.assertEqual(10, bits_to_int([1,0,1,0]))
+        self.assertEqual(2, bits_to_int([0,1,0]))
+        self.assertEqual(225, bits_to_int([1,1,1,0,0,0,0,1]))
+
+    def test_number_packet_example(self):
+        packet, remainder = Packet.make_packet_from_hex('D2FE28')
+        self.assertEqual(6, packet.version)
+        self.assertEqual(4, packet.type_id)
+        self.assertEqual(2021, packet.value())#
+
+    def test_operator_length_type_0(self):
+        packet, _ = Packet.make_packet_from_hex('38006F45291200')
+        self.assertEqual(1, packet.version)
+        self.assertEqual(6, packet.type_id)
+        self.assertEqual(2, len(packet.sub_packets()))
+
+    def test_operator_length_type_1(self):
+        packet, _ = Packet.make_packet_from_hex('EE00D40C823060')
+        self.assertEqual(7, packet.version)
+        self.assertEqual(3, packet.type_id)
+        self.assertEqual(3, len(packet.sub_packets()))
+
+    def test_version_sum_eg0(self):
+        packet, _ = Packet.make_packet_from_hex('D2FE28')
+        version_sum = packet.version_sum()
+        self.assertEqual(6, version_sum)
+
+    def test_version_sum_eg1(self):
+        packet, _ = Packet.make_packet_from_hex('8A004A801A8002F478')
+        version_sum = packet.version_sum()
+        self.assertEqual(16, version_sum)
+
+    def test_version_sum_eg2(self):
+        packet, _ = Packet.make_packet_from_hex('620080001611562C8802118E34')
+        version_sum = packet.version_sum()
+        self.assertEqual(12, version_sum)
+
+    def test_version_sum_eg3(self):
+        packet, _ = Packet.make_packet_from_hex('C0015000016115A2E0802F182340')
+        version_sum = packet.version_sum()
+        self.assertEqual(23, version_sum)
+
+    def test_version_sum_eg4(self):
+        packet, _ = Packet.make_packet_from_hex('A0016C880162017C3686B18A3D4780')
+        version_sum = packet.version_sum()
+        self.assertEqual(31, version_sum)
+
 
 if __name__ == '__main__':
     unittest.main()
